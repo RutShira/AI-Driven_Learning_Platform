533a9a03044dafba42bddacaeb214be320f036e4
Rutshi <rutshi054853@email.com>
2025-06-09 13:43:50 +0300

    build db and Entities

---
+++ backend/Dal/Models/databaseManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dal.Models;
+
+public partial class databaseManager : DbContext
+{
+    public databaseManager()
+    {
+    }
+
+    public databaseManager(DbContextOptions<databaseManager> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Prompt> Prompts { get; set; }
+
+    public virtual DbSet<SubCategory> SubCategories { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\User\\Desktop\\Programming Task\\AI-Driven Learning Platform\\backend\\Dal\\Database\\AI-DrivenDB.mdf\";Integrated Security=True;Connect Timeout=30");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__Categori__19093A0BB1F11D74");
+
+            entity.Property(e => e.Description).HasColumnType("text");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Prompt>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Prompts__3214EC0726753AC7");
+
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Prompt1).HasColumnName("Prompt");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Prompts)
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Prompts__Categor__440B1D61");
+
+            entity.HasOne(d => d.SubCategory).WithMany(p => p.Prompts)
+                .HasForeignKey(d => d.SubCategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Prompts__SubCate__44FF419A");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Prompts)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Prompts__UserId__4316F928");
+        });
+
+        modelBuilder.Entity<SubCategory>(entity =>
+        {
+            entity.HasKey(e => e.SubCategoryId).HasName("PK__SubCateg__26BE5B192D0120D4");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Category).WithMany(p => p.SubCategories)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK__SubCatego__Categ__3E52440B");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CC4CB41357ED");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D10534C333071A").IsUnique();
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Phone)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.Role)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasDefaultValue("User");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
